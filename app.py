a1aadd917c4ad426eca98f2f9b6f0a53102f2e4c
isha-rish <ishanbv7@gmail.com>
2026-08-01 23:16:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,5 @@ def save_user_data():
     return jsonify({"status": "success", "message": "Vault successfully synchronized."})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
